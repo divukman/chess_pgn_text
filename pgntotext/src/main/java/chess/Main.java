aecd3d782909b490fac02ec68b7f95ee44aa1185
divukman <divukman@gmail.com>
2019-07-27 19:44:09 +0200

    initial draft

---
+++ pgntotext/src/main/java/chess/Main.java
@@ -0,0 +1,48 @@
+package chess;
+
+import com.github.bhlangonijr.chesslib.game.Game;
+import com.github.bhlangonijr.chesslib.pgn.PgnHolder;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        int numOfGames = 1;
+
+//        if (args.length < 1) {
+//            throw new RuntimeException("Missing file path.");
+//        }
+
+        System.out.println();
+
+        PgnHolder pgn = new PgnHolder("C:\\Users\\Dimitar\\Dropbox\\Instrukcije\\171. lekcija\\domaci.pgn");//args[0]); //"C:\\Users\\Dimitar\\Dropbox\\Instrukcije\\171. lekcija\\domaci.pgn"
+        pgn.loadPgn();
+        for (Game game: pgn.getGame()) {
+
+            final String strWhitePlayerName = game.getWhitePlayer().getName();
+            final String strBlackPlayerName = game.getBlackPlayer().getName();
+
+            final String strEvent = game.getRound().getEvent().getName();
+            final String strEventDate = game.getDate();
+            final String strAnnotator = game.getAnnotator();
+            final String strResult = game.getResult().getDescription();
+            final String strFEN = game.getFen();
+
+
+            System.out.println("####################################################################################");
+            System.out.println(String.format("Broj: %d  %s - %s, %s %s [%s]", numOfGames, strWhitePlayerName, strBlackPlayerName, strEvent, strEventDate, strAnnotator));
+            System.out.println("-----------------");
+            System.out.println(String.format("FEN: %s", strFEN));
+            System.out.println("-----------------");
+            System.out.println(strResult);
+            System.out.println("-----------------");
+            System.out.println("Analiza:");
+            System.out.println("-----------------");
+            System.out.println("Nastavak:");
+
+            System.out.println();
+
+            ++numOfGames;
+        }
+    }
+
+}
